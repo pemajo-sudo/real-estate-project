0385102a54e131d636cd581af718c339e3cf7c04
Chamindri <chamindribdesilva@gmail.com>
2026-04-01 19:30:02 +0530

    Added URL route for inquiry feature

--- listings/urls.py
+++ listings/urls.py
@@ -13,3 +13,5 @@ urlpatterns = [
     path("properties/<int:pk>/edit/", views.property_update, name="property_update"),
     path("properties/<int:pk>/delete/", views.property_delete, name="property_delete"),
 ]
+
+path('property/<int:pk>/inquiry/', views.send_inquiry, name='send_inquiry'),
